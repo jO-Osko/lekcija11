2b5954ce0b64b39085aacb6186f6b31ed72cbdb0
Filip Koprivec <jO-Osko@users.noreply.github.com>
2016-11-05 19:12:55 +0100

    Urejanje/brisanje

--- contact.py
+++ contact.py
@@ -1,4 +1,3 @@
-
 class Contact(object):
 
     def __init__(self, first_name, last_name, phone_number, email, birth_year):
@@ -17,10 +16,7 @@ class Contact(object):
 
 moji_kontakti = [Contact("Filip", "Koprivec", "123456", "asd@gmail.com", 1900)]
 
-while True:
-    odgovor = raw_input("Ali zelis koncati?")
-    if odgovor == "da":
-        break
+def dodaj_kontakt():
     ime = raw_input("ime")
     priimek = raw_input("priimek")
     phone = raw_input("phone")
@@ -30,13 +26,86 @@ while True:
     kontakt = Contact(ime, priimek, phone, birth_year=year, email=email)
 
     moji_kontakti.append(kontakt)
-    
 
-for posamezni_kontakt in moji_kontakti:
-    #print(posamezni_kontakt.last_name + ", " +
-    #      posamezni_kontakt.first_name + ": " +
-    #      str(posamezni_kontakt.phone_number))
-    print(posamezni_kontakt.izpisi_kontakt())
+# Pomozna funkcija, s pomocjo katere uporabnik izbere kontakt, ki ga bo urejal/brisal
+def izberi_kontakt():
+    # Tabulator uporabimo zato, da se lepse poravnajo
+    izpisi_kontakte()
+    num = int(raw_input("vpisi zaporedno stevilko kontakta"))
+    # Preverimo, ali je vpisal smiselno stevilko
+    if not ( 0 <= num < len(moji_kontakti)):
+        print("Takega kontakta ni")
+        return -1
+    return num
+
+def izbrisi_kontakt():
+    num = izberi_kontakt()
+    if num == -1:
+        # Ni tega kontakta (To se sicer lepse resi z izjemami, a do sedaj jih se nismo vzeli)
+        return
+    # izbrisemo izbrani kontakt (s tem se kontakto za njim zmanjsajo zaporedne stevilke)
+    print("Brisem ....")
+    # Bliznjica, ce uporabimo ze napisano funkcijo, ki naredi tocno to, kar zelimo, izbrise element in nam ga vrne, da ga lahko se uporabimo
+    bliznjica = False
+    if bliznjica:
+        # python zna narediti vec stvari hkrati
+        izbrisani = moji_kontakti.pop(num)
+    else:
+        # Lahko pa to naredimo sami
+        # Vzamemo izbrisanega
+        izbrisani = moji_kontakti[num]
+        # Izbrisemo iz seznama (Opozorilo, ta operacija je lahko pri velikih seznamih pocasna, saj mora vse za njim premakniti za eno mesto nazaj - s tem se ni potrebno ubadati, samo za zanimivost)
+        del moji_kontakti[num]
+
+
+    print("Izbrisal: " + izbrisani.izpisi_kontakt())
+
+def uredi_kontakt():
+    num = izberi_kontakt()
+    if num == -1:
+        # Ni tega kontakta (To se sicer lepse resi z izjemami, a do sedaj jih se nismo vzeli)
+        return
+    # Podobno kot pri dodajanju, naredimo nov kontakt, le da ga tokrat vrnimemo na mesto (namesto) starega
+    print("Urejanje: " + moji_kontakti[num].izpisi_kontakt())
+    ime = raw_input("ime")
+    priimek = raw_input("priimek")
+    phone = raw_input("phone")
+    email = raw_input("email")
+    year = raw_input("year")
+
+    # Naredimo nov kontakt
+    kontakt = Contact(ime, priimek, phone, birth_year=year, email=email)
+
+    # Prepisemo star kontakt
+    moji_kontakti[num] = kontakt
+
+    # Lahko bi urejal kontakt tudi po kosih
+    # moji_kontkti[num].ime = raw_input("Novo ime")
+    # In tako naprej
+    # Ampak v kodo bi se ti na ta nacin hitro prikradla napaka, poskusi najti kje in zakaj
+
+def izpisi_kontakte():
+    print("St \t| Kontakt")
+    print("-"*50)
+    for j in range(len(moji_kontakti)):
+        print(str(j) + "\t| " + moji_kontakti[j].izpisi_kontakt())
+
+while True:
+    odgovor = raw_input("Povej kaj zelis narediti (dodaj, izbrisi, uredi, izpisi, koncaj)?").lower()
+    # Ideja je, da ima sedaj uporabnik ze na zacetku vec moznosti za izbor, kaj bi rad pocel
+    # (prej je imel na voljo zgolj eno - dodaj), zato ga najprej prasamo po zelji
+    if odgovor == "dodaj":
+        dodaj_kontakt()
+    elif odgovor == "uredi":
+        uredi_kontakt()
+    elif odgovor == "izbrisi":
+        izbrisi_kontakt()
+    elif odgovor == "izpisi":
+        izpisi_kontakte()
+    elif odgovor == "koncaj":
+        break
+    else:
+        print("Nisem razumel odgvora :(")
 
 
 # Izpisimo samo dolocene
@@ -78,7 +147,6 @@ with open("pickle_out", "wb") as pickle_datoteka:
 """
 with open("pickle_out", "rb") as pickle_datoteka:
     moji_kontakti_v_datoteki = pickle.load(pickle_datoteka)
-
 """
 
 # Super zanimivost
